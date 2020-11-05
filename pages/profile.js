e70b582db3fb61dba5087f1d9cff5438be9dc521
emsbn <emsbn1@gmail.com>
2020-11-06 00:35:17 +0900

    1-8

--- pages/profile.js
+++ pages/profile.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import AppLayout from '../components/AppLayout';
 import Head from 'next/head';
 
+import AppLayout from '../components/AppLayout';
+import NicknameEditForm from '../components/NicknameEditForm';
+import FollowList from '../components/FollowList';
+
 const Profile = () => {
+  const followingList = [{ nickname: '진돌이' }, { nickname: '임또깡' }, { nickname: '쨔니' }];
+  const followerList = [{ nickname: '진돌이' }, { nickname: '임또깡' }, { nickname: '쨔니' }];
+
   return (
     <>
       <Head>
         <title>내 프로필 | ReNeDuSaEx</title>
       </Head>
-      <AppLayout>프로필 페이지</AppLayout>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
+      </AppLayout>
     </>
   );
 };
