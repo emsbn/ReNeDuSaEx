2d72e266dfa74f01715c73867d2d221ce5760e6e
emsbn <emsbn1@gmail.com>
2020-11-12 00:03:38 +0900

    2-3

--- reducers/index.js
+++ reducers/index.js
@@ -10,28 +10,39 @@ const initialState = {
   },
 };
 
-// async action creator
-// action creator
-const changeNickname = (data) => {
+export const loginAction = (data) => {
   return {
-    type: 'CHANGE_NICKNAME',
+    type: 'LOG_IN',
     data,
   };
 };
-changeNickname('누렁이');
-// {
-//   type: 'CHANGE_NICKNAME',
-//   data: '누렁이'
-// }
-store.dispatch(changeNickname('누렁이'));
+
+export const logoutAction = () => {
+  return {
+    type: 'LOG_OUT',
+  };
+};
 
 // (이전상태, 액션) => 다음상태
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'CHANGE_NICKNAME':
+    case 'LOG_IN':
       return {
         ...state,
-        name: action.data,
+        user: {
+          ...state.user,
+          isLoggedIn: true,
+          user: action.data,
+        },
+      };
+    case 'LOG_OUT':
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          isLoggedIn: false,
+          user: null,
+        },
       };
   }
 };
