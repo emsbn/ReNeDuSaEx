bbaa2e22b9b6609c9d2ada082885084008371ecc
emsbn <emsbn1@gmail.com>
2020-11-11 18:37:26 +0900

    2-3

--- reducers/index.js
+++ reducers/index.js
@@ -1,14 +1,31 @@
 const initialState = {
-  name: 'emsbn',
-  age: 31,
-  password: 'pwdr',
+  user: {
+    isLoggedIn: false,
+    user: null,
+    signUpData: {},
+    loginData: {},
+  },
+  post: {
+    mainPosts: [],
+  },
 };
 
-const changeNickname = {
-  type: 'CHANGE_NICKNAME',
-  data: '누렁이',
+// async action creator
+// action creator
+const changeNickname = (data) => {
+  return {
+    type: 'CHANGE_NICKNAME',
+    data,
+  };
 };
+changeNickname('누렁이');
+// {
+//   type: 'CHANGE_NICKNAME',
+//   data: '누렁이'
+// }
+store.dispatch(changeNickname('누렁이'));
 
+// (이전상태, 액션) => 다음상태
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'CHANGE_NICKNAME':
