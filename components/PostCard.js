9398b55bea90135c9bcd1720728e2ead615499bd
emsbn <emsbn1@gmail.com>
2020-11-17 00:40:38 +0900

    2-7

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,9 +1,78 @@
-const PostCard = () => {
+import React, { useCallback, useState } from 'react';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { Card, Popover, Button, Avatar } from 'antd';
+import {
+  HeartOutlined,
+  MessageOutlined,
+  RetweetOutlined,
+  EllipsisOutlined,
+  HeartTwoTone,
+} from '@ant-design/icons';
+import PostImages from './PostImages';
+
+const PostCard = ({ post }) => {
+  const [liked, setLiked] = useState(false);
+  const [commentFormOpened, setCommendFormOpened] = useState(false);
+  const onToggleLike = useCallback(() => {
+    setLiked((prev) => !prev);
+  }, []);
+  const onToggleComment = useCallback(() => {
+    setCommendFormOpened((prev) => !prev);
+  }, []);
+
+  const id = useSelector((state) => state.user.me?.id);
   return (
-    <>
-      <div>PostCard</div>
-    </>
+    <div style={{ marginBottom: 20 }}>
+      <Card
+        cover={post.images[0] && <PostImages images={post.images} />}
+        actions={[
+          <RetweetOutlined key="retweet" />,
+          liked ? (
+            <HeartTwoTone twoToneColor="#eb2f96" key="heart" onClick={onToggleLike} />
+          ) : (
+            <HeartOutlined key="heart" onClick={onToggleLike} />
+          ),
+          <MessageOutlined key="comment" onClick={onToggleComment} />,
+          <Popover
+            key="more"
+            content={
+              <Button.Group>
+                {id && post.user.id === id ? (
+                  <>
+                    <Button>수정</Button>
+                    <Button type="danger">삭제</Button>
+                  </>
+                ) : (
+                  <Button>신고</Button>
+                )}
+              </Button.Group>
+            }>
+            <EllipsisOutlined />
+          </Popover>,
+        ]}>
+        <Card.Meta
+          avatar={<Avatar>{post.user.nickname[0]}</Avatar>}
+          title={post.user.nickname}
+          description={post.content}
+        />
+      </Card>
+      {commentFormOpened && <div>댓글 부분</div>}
+      {/*<CommentForm />*/}
+      {/*<Comments />*/}
+    </div>
   );
+};
+
+PostCard.propTypes = {
+  post: PropTypes.shape({
+    id: PropTypes.number,
+    user: PropTypes.object,
+    content: PropTypes.string,
+    images: PropTypes.arrayOf(PropTypes.object),
+    comments: PropTypes.arrayOf(PropTypes.object),
+    createdAt: PropTypes.string,
+  }),
 };
 
 export default PostCard;
--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 import 'antd/dist/antd.css';
-import wrapper from '../store/configureStore'
+import wrapper from '../store/configureStore';
 
 const ReNeDuSaEx = ({ Component }) => {
   return (
