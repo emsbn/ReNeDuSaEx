0b35fa545d2f12a20b2c56e6d2b8c7faff07950d
emsbn <emsbn1@gmail.com>
2020-11-18 20:02:43 +0900

    3-1

--- reducers/user.js
+++ reducers/user.js
@@ -5,28 +5,28 @@ export const initialState = {
   loginData: {},
 };
 
-export const loginAction = (data) => {
+export const loginRequestAction = (data) => {
   return {
-    type: 'LOG_IN',
+    type: 'LOG_IN_REQUEST',
     data,
   };
 };
 
-export const logoutAction = () => {
+export const logoutRequestAction = () => {
   return {
-    type: 'LOG_OUT',
+    type: 'LOG_OUT_REQUEST',
   };
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'LOG_IN':
+    case 'LOG_IN_REQUEST':
       return {
         ...state,
         isLoggedIn: true,
         me: action.data,
       };
-    case 'LOG_OUT':
+    case 'LOG_OUT_REQUEST':
       return {
         ...state,
         isLoggedIn: false,
