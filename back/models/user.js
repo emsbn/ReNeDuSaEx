b98302fd28f2e2c133836da4b51b277c850e5540
emsbn <emsbn1@gmail.com>
2020-12-30 23:59:15 +0900

    4-7

--- back/models/user.js
+++ back/models/user.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
   User.associate = (db) => {
     db.User.hasMany(db.Post);
     db.User.hasMany(db.Comment);
-    db.User.belongsToMany(db.Post, { throught: 'Like', as: 'Liked' });
+    db.User.belongsToMany(db.Post, { through: 'Like', as: 'Liked' });
     db.User.belongsToMany(db.User, { through: 'Follow', as: 'Followers', foreignKey: 'FollowingId' });
     db.User.belongsToMany(db.User, { through: 'Follow', as: 'Followings', foreignKey: 'FollowerId' });
   };
